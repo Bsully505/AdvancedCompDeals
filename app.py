acc938ad14b1edb095c706b5c6e4d577edcc4195
Bsully505 <bsully505@yahoo.com>
2022-05-01 19:01:03 -0400

    Added new functions and cleaned up with colin

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask,render_template,request
 from dotenv import load_dotenv
 import json
 import requests
-
+from html.parser import HTMLParser
 
 
 
@@ -16,18 +16,37 @@ load_dotenv()
 App=Flask(__name__)
 
 @App.route('/GetRestResponse',methods=['GET'])
-def getResponce():
-    print("Hello")
+def getResponse():
     query = request.args.get('keyWord')
     Time = request.args.get('time')
-    headers = { 'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Mobile Safari/537.36'}
-    #req = requests.request(url='https://developer.woot.com/feed/Wootoff?page=1',method='get',headers= headers)
-    req = requests.get(f'https://dealsea.com/search?search_mode=Deals&q={query}',headers= headers)
-    return req.text
+    callResponse = CallAPI(query,10)
+    res = ParseHTML(callResponse.text)
+    #parse req.text and then send back 
+    return res
     
     #this is where we are going to get the request command and then return the json responce 
     
 
+#Henoks part is to parse using req test and be able to send it to front end 
+def ParseHTML(str):
+    # use this library to parse out the desired items HTMLParser 
+    # and then return the html 
+    reader = HTMLParser()
+    reader.feed(str)
+    return str
+
+
+#this is to determine how many calls we need to send out to dealsea with increments of 10 
+def DetermineAmtOfCalls(totalNum):
+    res = totalNum/10
+    res+=1
+    return res
+        
+        
+def CallAPI(query, startIndex):
+    headers = { 'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Mobile Safari/537.36'}
+    req = requests.get(f'https://dealsea.com/search?search_mode=Deals&q={query}&n={startIndex}',headers= headers)
+    return req
 
     
 
